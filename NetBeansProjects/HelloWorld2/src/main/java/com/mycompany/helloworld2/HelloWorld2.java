2abdf86312e41574c400d50e44f26b0afc5a98ab
ayub <ayub@ayubs-air>
2023-01-29 19:06:35 -0500

    hello world push to netbean example

---
+++ NetBeansProjects/HelloWorld2/src/main/java/com/mycompany/helloworld2/HelloWorld2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld2;
+
+/**
+ *
+ * @author ayub
+ */
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
