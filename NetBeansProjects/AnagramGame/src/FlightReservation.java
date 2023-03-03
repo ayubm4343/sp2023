a7c8afba9077b2a847b688182b41d47334634736
ayub <ayub@ayubs-air>
2023-03-02 23:25:32 -0500

    hello world push to netbean example

---
+++ NetBeansProjects/AnagramGame/src/FlightReservation.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ayub
+ */
+import java.util.Scanner;
+
+public class FlightReservation {
+    public static boolean[] seats = new boolean[10];
+    public static void  main( String[] args){
+        Scanner Scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Please type 1 for First Class and Please type 2 for Economy.");
+            int section = input.nextInt();
+            
+            if (section == 1) {
+    }
+}
